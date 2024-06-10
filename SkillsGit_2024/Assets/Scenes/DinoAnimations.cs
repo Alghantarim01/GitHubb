af9f852be7d1002a0031d98c4e41159f8f3588f5
Alghantarim01 <alghantari.m01@elliotthudsoncollege.ac.uk>
2024-06-10 12:09:19 +0100

    slide 17 walk

---
+++ SkillsGit_2024/Assets/Scenes/DinoAnimations.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoAnimations : MonoBehaviour {
+
+	Animator MyAnimator;
+	void Start () {
+		MyAnimator = GetComponent<Animator> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+			MyAnimator.SetBool ("walk", true);
+		} 
+		else 
+		{
+			MyAnimator.SetBool ("walk", false);
+		}
+	}
+}
