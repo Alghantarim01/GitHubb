857b93075e5aa932839be1cb830cff2840191915
Alghantarim01 <alghantari.m01@elliotthudsoncollege.ac.uk>
2024-06-25 15:51:04 +0100

    task 2 completed

--- SkillsGit_2024/Assets/scripts/HeroMovement.cs
+++ SkillsGit_2024/Assets/scripts/HeroMovement.cs
@@ -15,14 +15,20 @@ public class   HeroMovement : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow)) 
-		{
-				transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * speed, 0, 0);
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * speed, 0, 0);
 		}
 
-		if (Input.GetKeyDown(KeyCode.UpArrow)) 
-		{
-				rb.AddForce(Vector2.up * 400);
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			rb.AddForce (Vector2.up * 400);
 		}
 	}
-}
+	
+
+		void resetPosition() 
+		{
+			Debug.Log("SPIKED RECEIVED!");
+			transform.SetPositionAndRotation( new Vector3 (-5.58f,0.6f,0),Quaternion.identity);
+		}
+
+	}
--- SkillsGit_2024/Assets/scripts/Spikes.cs
+++ SkillsGit_2024/Assets/scripts/Spikes.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spikes : MonoBehaviour {
-			GameObject Hero;
+	      GameObject Hero;
+	      // Use this for initialization
+	      void Start () {
+		            Hero = GameObject.FindGameObjectWithTag("Player");
+		      }
+	      
+	      // Update is called once per frame
+	      void Update () {
+		            
+		      }
 
-	void Start () {
-				Hero = GameObject.FindGameObjectWithTag("Player");
-			}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	private void OnCollisionEnter2D(Collision2D other) {
+	      private void OnCollisionEnter2D(Collision2D other) {
 
-		Debug.Log("SPIKED!");
-		Hero.SendMessage("resetPosition");
-	}
-
+		            Debug.Log ("SPIKED!");
+		            Hero.SendMessage("resetPosition");
+		      }
 
 }
