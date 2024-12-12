0e8a950e3b8e5db9159b03ffdc7915c6cb4a6c18
Alghantarim01 <alghantari.m01@elliotthudsoncollege.ac.uk>
2024-12-12 11:57:23 +0000

    sprint one update

---
+++ NEA2024/Assets/scripts/LevelSelection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //this allows me to use the unity built in scene manager to swaps between scenes 
+
+
+
+public class LevelSelection : MonoBehaviour {
+
+	public void SelectLevel1()
+	{
+		SceneManager.LoadScene("Level1Scene");
+	}
+
+
+	public void SelectLevel2()
+	{
+		SceneManager.LoadScene("Level2Scene");
+	}
+
+
+	public void SelectLevel3()
+	{
+		SceneManager.LoadScene("Level3Scene");
+	}
+}
