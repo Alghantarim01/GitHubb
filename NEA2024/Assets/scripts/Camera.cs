077288f8f85bfcbb1494cada001f254b6ba18241
Alghantarim01 <alghantari.m01@elliotthudsoncollege.ac.uk>
2025-02-06 11:52:38 +0000

    animations and movemnt

---
+++ NEA2024/Assets/scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	public GameObject HeroPlayer;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3 (HeroPlayer.transform.position.x, HeroPlayer.transform.position.y + 2, -6.25f);
+	}
+}
