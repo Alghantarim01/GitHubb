d893960e9d2856fe38e8b32ff7d33e6855bfa80a
Alghantarim01 <alghantari.m01@elliotthudsoncollege.ac.uk>
2025-02-10 10:37:03 +0000

    ground check

--- NEA2024/Assets/scripts/HeroController.cs
+++ NEA2024/Assets/scripts/HeroController.cs
@@ -7,14 +7,26 @@ public class HeroController : MonoBehaviour {
 	Rigidbody2D rb;
 	float speed;
 	bool facingRight;
+	private bool Jumped;
+	public bool isGrounded;
 
 	void Start () {
 		speed = 5f;
 		rb = GetComponent<Rigidbody2D> ();
 		facingRight = true;
 	}
-	
 
+	void OnCollisionStay()
+	{
+		isGrounded = true;
+	}
+	void update()
+	{
+		if (Input.GetKey (KeyCode.Space) || (Input.GetKey (KeyCode.UpArrow)))
+		{
+			Jumped = true;
+		}
+	}
 	void FixedUpdate ()
 	{
 		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow)) {
@@ -33,6 +45,15 @@ public class HeroController : MonoBehaviour {
 		flip ();
 		} else if (move < 0 && facingRight) {
 			flip ();
+		}
+
+
+
+		if(Jumped && isGrounded)
+		{
+			rb.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+			Jumped = false;
+			isGrounded = true;
 		}
 	}
 
