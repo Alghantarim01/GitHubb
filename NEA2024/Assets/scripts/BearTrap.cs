f2ee81d79a72f7d83e0ec0f59fce3ff7e5629d40
Alghantarim01 <alghantari.m01@elliotthudsoncollege.ac.uk>
2025-02-24 07:42:14 +0000

    sprint 4

--- NEA2024/Assets/scripts/BearTrap.cs
+++ NEA2024/Assets/scripts/BearTrap.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class BearTrap : MonoBehaviour {
 
+	GameObject Player;
+	Animator MyAnimator;
+
 	// Use this for initialization
 	void Start () {
-		
+		Player = GameObject.FindGameObjectWithTag ("HeroPlayer");
+		MyAnimator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	IEnumerator Respawn(GameObject Player)
+	{
+		yield return new WaitForSeconds (0.3f); // wait 1 second
+		Player.SendMessage ("resetPosition");
+	}
+
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.name == "HeroPlayer")
+		{
+			MyAnimator.SetTrigger ("attack");
+			StartCoroutine (Respawn (collision.gameObject));
+		}
 	}
 }
