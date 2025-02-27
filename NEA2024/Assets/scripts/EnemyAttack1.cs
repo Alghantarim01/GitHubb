ac76ddb1abcda719347504fb474d541373303ea7
Alghantarim01 <alghantari.m01@elliotthudsoncollege.ac.uk>
2025-02-27 06:25:45 +0000

    sprint 4

---
+++ NEA2024/Assets/scripts/EnemyAttack1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack1 : MonoBehaviour {
+
+	Animator EnemyAnimator;
+	GameObject Player;
+
+
+
+	// Use this for initialization
+	void Start () {
+		EnemyAnimator = GetComponent<Animator> ();
+		Player = GameObject.FindGameObjectWithTag ("HeroPlayer");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.name == "HeroPlayer")
+		{
+			EnemyAnimator.Play ("MSAttack");
+			Debug.Log ("reduce player health");
+		}
+	}
+	
+}
