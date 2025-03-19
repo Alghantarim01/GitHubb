7fa2498e9f2a62410b4c0460fd398c2d464659de
Alghantarim01 <alghantari.m01@elliotthudsoncollege.ac.uk>
2025-03-19 12:03:55 +0000

    .

--- NEA2024/Assets/scripts/MainMenu.cs
+++ NEA2024/Assets/scripts/MainMenu.cs
@@ -49,6 +49,13 @@ public class MainMenu : MonoBehaviour
 			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 		}
 	}
+	public void ReplayLevel()
+	{
+		if (SceneManager.GetActiveScene ().buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 0);
+		}
+	}
 
 
 	public void volumeSlider (float volume)
